13a136a5a812fb79beaf0fb3cbe4c2cff4e4e986
sukteMung <tur58332@temple.edu>
2026-02-05 13:48:11 -0500

    Working transactions. Next: make it look better

--- src/pages/dashboard/Transactions.jsx
+++ src/pages/dashboard/Transactions.jsx
@@ -1,28 +1,31 @@
 import { TransactionsFunctions } from "../../context/TransactionContext";
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 export default function Transactions() {
   const { loading, getRecentTransactions } = TransactionsFunctions();
 
-  const [transactions, setTransactions] = useState()
+  const [transactions, setTransactions] = useState([]);
 
   useEffect(() => {
     if (loading) return; // wait until context has a user
 
     const fetchTransactions = async () => {
-      console.log('fetching')
       const newTransactions = await getRecentTransactions();
-      setTransactions(newTransactions)
-      console.log("Transactions after set: ", newTransactions)
+      setTransactions(newTransactions);
+      console.log("Transactions are set: ", newTransactions);
     };
 
     fetchTransactions();
   }, [loading]);
 
   return (
-    < div >
-      {/* {Object.values().map(([key, value]) => {
-        return <div> Test {key} </div>
-      })} */}
-    </div >
-  )
+    <div>
+      {transactions.map((t, i) => (
+        <div key={t.id}>
+          <p>Description: {t.description}</p>
+          <p>Amount: {t.amount}</p>
+          <p>Type: {t.type}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
